7b64716a411c7142a118c7b8b6183a2464f4008e
Robin Siep <RobinSiep1@gmail.com>
2019-12-18 12:28:52 +0100

    websocket machines

--- src/pages/Machines.jsx
+++ src/pages/Machines.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
+import openSocket from 'socket.io-client';
 
-export default function Machines() {
-  return <div>Machines</div>; 
+const  socket = openSocket('http://localhost:8080');
+
+export default class Machines extends React.PureComponent {
+  componentDidMount() {
+    socket.on('machines', machines => console.log(machines));
+  }
+
+  render() {
+    return <div>Machines</div>; 
+  }
 }
