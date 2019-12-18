4022d15dd1d8ec0ae157f6cbd679f9820d6d3030
Robin Siep <RobinSiep1@gmail.com>
2019-12-18 11:11:49 +0100

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,41 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch} from 'react-router-dom';
+import styled, { createGlobalStyle, ThemeProvider } from 'styled-components';
+import Machines from './pages/Machines';
+
+const theme = {
+  primaryColor: "#23242c"
+};
+
+const GlobalStyle = createGlobalStyle`
+  html, body {
+    margin: 0px;
+    padding: 0px;
+    height: 100%;
+    width: 100%;
+  }
+`;
+
+const Content = styled.div`
+  height: 100%;
+  width: 100%;
+`;
 
 export default function App() {
-  return <div></div>;
+  return (
+    <div>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Content>
+            <Switch>
+              <Route exact
+                path="/"
+                component={Machines} />
+            </Switch>
+            <GlobalStyle />
+          </Content>
+        </BrowserRouter>
+      </ThemeProvider>
+    </div>
+  );
 }
