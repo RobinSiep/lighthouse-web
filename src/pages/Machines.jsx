9843e3375ff0178607d6c207f9686a208bb6a72b
Robin Siep <RobinSiep1@gmail.com>
2019-12-18 13:07:07 +0100

    add machines to state

--- src/pages/Machines.jsx
+++ src/pages/Machines.jsx
@@ -4,11 +4,19 @@ import openSocket from 'socket.io-client';
 const  socket = openSocket('http://localhost:8080');
 
 export default class Machines extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      machines: []
+    }
+  }
+
   componentDidMount() {
-    socket.on('machines', machines => console.log(machines));
+    socket.on('machines', machines => this.setState({ machines: machines }));
   }
 
   render() {
-    return <div>Machines</div>; 
+    console.log(this.state.machines);
+    return <div></div>; 
   }
 }
