b56d135c1318da7615289f1f68cfc58dbc7b509f
Robin Siep <RobinSiep1@gmail.com>
2020-04-22 12:21:46 +0200

    styling and status indicator

--- src/components/MachineListItem.jsx
+++ src/components/MachineListItem.jsx
@@ -6,13 +6,33 @@ const propTypes = {
   machine: PropTypes.object.isRequired
 };
 
-const ListItem = styled.div``;
+const ListItem = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 8px;
+  width: 100%;
+  border: 1px solid black;
+
+  > * {
+    margin: 0px 8px 0px 0px;
+  }
+`;
+
+const Indicator = styled.span`
+  height: 10px;
+  width: 10px;
+  background-color: ${props => props.color};
+  border-radius: 50%;
+`;
 
 function MachineListItem(props) {
+  let online = "sys_info" in props.machine;
+  let color = online ? '#15CD72' : '#FF0000';
+
   return (
-    <ListItem>
+    <ListItem className={props.className}>
+      <Indicator color={color} />
       <h4>{props.machine.name}</h4>
-      <p>{("sys_info" in props.machine) ? 'Online' : 'Offline'}</p>
     </ListItem>
   );
 }
